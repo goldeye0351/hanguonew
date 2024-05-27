41f5d5d2c12d901ccab5b9b23af970e760b9e30c
Goldeye0351 <goldeye0351@gmail.com>
2024-05-27 09:17:57 +0800

    del cover2

--- src/components/posts/post-card.tsx
+++ src/components/posts/post-card.tsx
@@ -7,23 +7,13 @@ import CategoryList from '@/components/category-list';
 import { Post } from '@/types/post';
 
 export default function PostCard({
-  post: { slug,emoji,link,summary, type,title, date, cover, categories, blurUrl },
+  post: { slug,emoji,link,summary, type,title, date,  categories, blurUrl },
 }: {
   post: Post;
 }) {
   return (
     <Link href={`/blog/${slug}`}>
       <article className="mx-auto flex max-w-[25rem] flex-col overflow-hidden rounded-xl shadow-xl shadow-gray-400 transition-all duration-300 hover:scale-[1.05] hover:shadow-2xl dark:shadow-black">
-        <div className="relative h-60">
-          <Image
-            src={cover}
-            alt="cover image"
-            fill
-            style={{ objectFit: 'cover' }}
-            placeholder="blur"
-            blurDataURL={blurUrl}
-          />
-        </div>
         {link}{type}
         <div className="flex h-48 flex-col p-4">
           <h3 className="line-clamp-2 h-16 text-2xl font-bold">{emoji}{title}</h3>
